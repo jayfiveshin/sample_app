463e5e095131f56667fce78782991a6253df3c01
Joonha Shin <shin.joonha@gmail.com>
2012-11-07 19:15:19 -0500

    Changes back to original
    Pages did not render properly.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  # Returns the full title on a per-page basis
-  def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
-      base_title
-    else
-      "#{base_title} | #{page_title}"
-    end
-  end
 end
